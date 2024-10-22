1aa5516c211a6212248659ce2ed726325c316018
JOSERSU <90009804+JOSERSU@users.noreply.github.com>
2024-10-22 00:59:28 -0600

    describe changes

--- src/main/java/com/upiiz/ecommerce/controllers/OrdenesClientesController.java
+++ src/main/java/com/upiiz/ecommerce/controllers/OrdenesClientesController.java
@@ -1,4 +1,20 @@
 package com.upiiz.ecommerce.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/clientes/{clienteId}/ordenes")
 public class OrdenesClientesController {
+    // GET - Obtener todas las ordenes de un cliente
+    @GetMapping
+    public String listarFacturasDeCliente(@PathVariable int clienteId) {
+        return "Muestra todas las facturas del cliente con ID: " + clienteId;
+    }
+
+    // GET - Obtener una orden específica de un cliente
+    @GetMapping("/{orden_id}")
+    public String listarFacturaDeCliente(@PathVariable int clienteId, @PathVariable int orden_id) {
+        return "Muestra la orden con ID: " + orden_id + " del cliente con ID: " + clienteId;
+    }
 }
+
